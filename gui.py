28a87f6f64dfb0d012902e25f10098e252b4c7aa
JamesSharman <jamesecsharman@gmail.com>
2022-12-30 09:31:39 +0000

    Implemented Add button & close

--- gui.py
+++ gui.py
@@ -1,12 +1,37 @@
 from modules import functions
 import PySimpleGUI as Gui
 
-label = Gui.Text(text="Type in a to-do: ", font='bold', text_color='white', background_color='grey')
-input_box = Gui.InputText(tooltip="Enter a to-do")
-add_button = Gui.Button(button_text="Add", font='bold', button_color="black")
-quit_button = Gui.Button(button_text="Quit", font='bold', button_color="black")
+label = Gui.Text(text="Type in a to-do: ",
+                 font=('Verdana', 15),
+                 text_color='white',
+                 background_color='grey')
+input_box = Gui.InputText(tooltip="Enter a to-do",
+                          key="todo")
+add_button = Gui.Button(button_text="Add",
+                        font='bold',
+                        button_color="black")
+quit_button = Gui.Button(button_text="Quit",
+                         font='bold',
+                         button_color="black")
 
-window = Gui.Window(background_color='grey', title='My To-Do App',
-                    layout=[[label], [input_box, add_button], [quit_button]])
-window.read()
+window = Gui.Window(background_color='grey',
+                    title='My To-Do App',
+                    layout=[[label],
+                            [input_box, add_button],
+                            [quit_button]],
+                    font=('Verdana', 15))
+
+while True:
+    event,values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case Gui.WIN_CLOSED:
+            break
+
 window.close()
