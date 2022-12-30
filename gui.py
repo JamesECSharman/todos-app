3283e0fe92d2519076dc38ca3e88b9040713ea60
JamesSharman <jamesecsharman@gmail.com>
2022-12-30 16:51:09 +0000

    Added functional clear button, added columns and updated the Gui layout

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ label = Gui.Text(text="Type in a to-do: ",
                  font=('Verdana', 15),
                  text_color='white',
                  background_color='grey')
-input_box = Gui.InputText(tooltip="Enter a to-do",
+input_box = Gui.InputText(tooltip="Enter a to-do", size=(46, 0),
                           key="todo")
 add_button = Gui.Button(button_text="Add",
                         font='bold',
@@ -20,17 +20,26 @@ complete_button = Gui.Button(button_text="Complete",
                              font='bold',
                              button_color="black"
                              )
+clear_button = Gui.Button(button_text="Clear",
+                          font='bold',
+                          button_color="black"
+                          )
 list_box = Gui.Listbox(values=functions.get_todos(),
                        key='todos', enable_events=True,
                        size=[45, 10], background_color='light grey')
+white_space = Gui.Text(text="", size=(0, 3), background_color='grey')
 
-window_layout = [[label], [input_box, add_button], [list_box, [edit_button, complete_button]], [quit_button]]
+window_column_1 = [[label], [input_box], [list_box]]
+window_column_2 = [[add_button], [edit_button], [complete_button], [clear_button], [white_space], [quit_button]]
+
+layout = [[Gui.Column(window_column_1, element_justification='l', background_color='grey'),
+           Gui.Column(window_column_2, element_justification='l', vertical_alignment='bottom', background_color='grey')]]
 
 window = Gui.Window(background_color='grey',
                     title='My To-Do App',
                     sbar_trough_color='black',
                     sbar_background_color='Dark Grey',
-                    layout=window_layout,
+                    layout=layout,
                     font=('Verdana', 15))
 
 while True:
@@ -60,6 +69,12 @@ while True:
             todos = functions.get_todos()
             index = todos.index(todo_to_complete)
             removed_todo = todos.pop(index).strip('\n')
+            functions.write_todos(todos_arg=todos)
+            window['todos'].update(values=todos)
+
+        case 'Clear':
+            todos = functions.get_todos()
+            todos.clear()
             functions.write_todos(todos_arg=todos)
             window['todos'].update(values=todos)
 
