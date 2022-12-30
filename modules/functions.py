72b804e27c0b0bc2d131ef5fb2e11fb200ef3ccf
JamesSharman <jamesecsharman@gmail.com>
2022-12-30 09:31:30 +0000

    Implemented Add button & close

--- modules/functions.py
+++ modules/functions.py
@@ -2,14 +2,16 @@ FILEPATH = "files/todos.txt"
 
 
 def get_todos(filepath=FILEPATH):
-    """ This function reads a text file and returns the list of to-do items """
+    """ This function reads a text file and
+    returns the list of to-do items """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, filepath=FILEPATH):
-    """ This function writes a text file, it is used to update the text file with the updated or new to-dos """
+    """ This function writes a text file, it is used to
+    update the text file with the updated or new to-dos """
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
 
