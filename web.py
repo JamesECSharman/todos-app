f0156acd85dff1a0b814fe36e9198ae11db5b29f
JamesSharman <jamesecsharman@gmail.com>
2023-01-02 09:13:40 +0000

    Implemented remove todo when checked feature

--- web.py
+++ web.py
@@ -16,8 +16,13 @@ st.title("My ToDo App")
 st.subheader("This is my ToDo Web App.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index).strip('\n')
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add New ToDo...", on_change=add_todo,
               key='new_todo')
